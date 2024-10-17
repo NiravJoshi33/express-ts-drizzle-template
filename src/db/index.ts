9016eb1a400005df8904673a30afe581a5183ad5
Nirav Joshi <niravjoshi3000@gmail.com>
2024-10-17 08:44:42 +0530

    connect to db before init server

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,3 +3,14 @@ import { DATABASE_URL } from "../utils/env-vars";
 import { dbSchema } from "./schema";
 
 const db = drizzle(DATABASE_URL, { schema: dbSchema });
+
+const connectToDb = async () => {
+  try {
+    await db.select().from(dbSchema.users); // Test the connection
+    console.log("Connected to the database");
+  } catch (error) {
+    console.error("Failed to connect to the database:", error);
+  }
+};
+
+export { db, connectToDb };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import app from "./app";
 import { PORT } from "./utils/env-vars";
+import { connectToDb } from "./db";
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+connectToDb()
+  .then(() => {
+    console.log("Connected to the database");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to connect to the database:", error);
+    process.exit(1);
+  });
