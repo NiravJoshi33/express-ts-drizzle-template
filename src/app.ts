991c565750da13bc1ba93d874ae9668d0d272658
Nirav Joshi <niravjoshi3000@gmail.com>
2024-10-17 21:57:41 +0530

    some security improvements

--- src/app.ts
+++ src/app.ts
@@ -3,20 +3,25 @@ import cors from "cors";
 import helmet from "helmet";
 import rateLimit from "express-rate-limit";
 import cookieParser from "cookie-parser";
-import { COOKIE_SECRET } from "./utils/env-vars";
+import { COOKIE_SECRET, NODE_ENV } from "./utils/env-vars";
+import { corsOptions } from "./utils/config";
 
 const app = express();
 
 // Middleware
-app.use(cors());
+
 app.use(helmet());
 app.use(cookieParser(COOKIE_SECRET));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
+app.use(express.json({ limit: "1mb" }));
+app.use(express.urlencoded({ extended: true, limit: "1mb" })); // Parse URL-encoded bodies
+
+app.use(cors(corsOptions));
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // limit each IP to 100 requests per windowMs
+  standardHeaders: true,
+  legacyHeaders: false,
 });
 
 app.use(limiter);
---
+++ src/utils/config.ts
@@ -0,0 +1,11 @@
+import { CorsOptions } from "cors";
+import { NODE_ENV } from "./env-vars";
+
+export const corsOptions: CorsOptions = {
+  origin:
+    NODE_ENV === "production"
+      ? "https://your-frontend-app.com"
+      : "http://localhost:3000",
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
